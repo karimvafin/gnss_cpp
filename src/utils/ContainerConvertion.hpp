8e30dd28dae4bbc33528330dbd725fee631ebf33
Arseniy <plakhotniuk.av@phystech.edu>
2024-03-15 15:17:58 +0300

    обработал некорректные данные

--- src/utils/ContainerConvertion.hpp
+++ src/utils/ContainerConvertion.hpp
@@ -7,10 +7,7 @@
 #include "third_party/eigen/Eigen/Core"
 namespace gnss::utils{
 inline Eigen::Vector3d stlToEigenVector(const std::vector<double> &stlVector) {
-    if(stlVector.size() != 3){
-        return Eigen::Vector3d::Zero();
-    }
-    const int vecSize = stlVector.size();
+    const unsigned int vecSize = stlVector.size();
     Eigen::VectorXd vec(vecSize);
     for(int i = 0; i < vecSize; ++i) {
         vec[i] = stlVector[i];
--- src/utils/Parsers.hpp
+++ src/utils/Parsers.hpp
@@ -47,18 +47,21 @@ namespace gnss::utils{
         std::vector<Sp3Data> epochsData;
         epochsData.reserve(jsonData.size());
         uint satNum;
+        std::vector<double> pos;
         for(auto it = jsonData.begin(), itend = jsonData.end(); it != itend; ++it){
             auto epochData = it.value();
             satNum = epochData["sv"].size();
             std::vector<std::pair<std::string, Eigen::Vector3d>> satelliteData;
             satelliteData.reserve(satNum);
             for(uint i = 0; i < satNum; ++i){
-                satelliteData.emplace_back(
-                    epochData["sv"][i].get<std::string>(),
-                    utils::stlToEigenVector(epochData["position"][i].get<std::vector<double>>()));
+                pos = epochData["position"][i].get<std::vector<double>>();
+                if(pos.size() == 3) {
+                    satelliteData.emplace_back(
+                        epochData["sv"][i].get<std::string>(),
+                        utils::stlToEigenVector(pos));
+                }
             }
             epochsData.push_back({.timeJD = strToJd(it.key()).value(), .satelliteData = satelliteData});
-
         }
         return epochsData;
     }
