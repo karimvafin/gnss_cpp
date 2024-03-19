603e4d679de82662fb1e941f3c52a9933753fcc5
vladimir chiniaev <chiniaev.vn@mipt.ru>
2024-03-20 00:02:42 +0300

    Разделил файл

--- src/cycle_slip_detection/CycleSlipDetection.hpp
+++ src/cycle_slip_detection/CycleSlipDetection.hpp
@@ -5,70 +5,34 @@
 #ifndef GNSS_CPP_CYCLESLIPDETECTION_HPP
 #define GNSS_CPP_CYCLESLIPDETECTION_HPP
 
-#include <deque>
+#include "CycleSlipData.hpp"
+
 #include <cmath>
+#include <deque>
+#include <vector>
 
 class SlidingWindow {
 private:
     unsigned int maxSize_;
     unsigned int currentSize_;
     std::deque<double> slidingWindow_;
+
 public:
     explicit SlidingWindow(const unsigned int maxSize) : maxSize_(maxSize), currentSize_(0) {}
 
-    void push_front(const double el) {
-        if (currentSize_ > maxSize_) {
-            slidingWindow_.pop_back();
-            slidingWindow_.push_front(el);
-        } else {
-            slidingWindow_.push_front(el);
-            currentSize_++;
-        }
-    }
+    void push_front(const double el);
 
-    [[nodiscard]] double getCurrentSize() const {
-        return currentSize_;
-    }
+    [[nodiscard]] double getCurrentSize() const;
 
-    void setCurrentSize(const unsigned int newCurrentSize) {
-        currentSize_ = newCurrentSize;
-    }
+    void setCurrentSize(const unsigned int newCurrentSize);
 
-    double getSum() {
-        double sum = 0;
-        for (const auto el : slidingWindow_) {
-            sum += el;
-        }
-        return sum;
-    }
+    [[nodiscard]] double getSum() const;
 
-    double getSumOfSquares() {
-        double sum = 0;
-        for (const auto el : slidingWindow_) {
-            sum += el * el;
-        }
-        return sum;
-    }
+    [[nodiscard]] double getSumOfSquares() const;
 };
 
-CycleSlipData determineCycleSlip(const std::vector<double> pseudorange,
-                                 const std::vector<double> carrierPhase,
-                                 const std::vector<double> epochs,
-                                 const double tolDeltaT) {
-    SlidingWindow slidingWindow(100);
-    for (unsigned int i = 1; i < pseudorange.size() - 1; i++) {
-        if (epochs[i] - epochs[i - 1] > tolDeltaT) {
-            return {1, epochs[i], true};
-        }
-        slidingWindow.push_front(carrierPhase[i] - pseudorange[i]);
-        const double mean = slidingWindow.getSum() / slidingWindow.getCurrentSize();
-        const double meanSquares = slidingWindow.getSumOfSquares() / slidingWindow.getCurrentSize();
-        const double S = std::sqrt(meanSquares - mean * mean);
-        if (std::abs(carrierPhase[i] - pseudorange[i] - mean) > 6 * S) {
-            return {1, epochs[i], true};
-        }
-    }
-    return {std::nullopt, std::nullopt, false};
-}
+[[nodiscard]] CycleSlipData determineCycleSlip(const std::vector<double> pseudorange,
+                                               const std::vector<double> carrierPhase, const std::vector<double> epochs,
+                                               const double tolDeltaT) noexcept;
 
 #endif  // GNSS_CPP_CYCLESLIPDETECTION_HPP
